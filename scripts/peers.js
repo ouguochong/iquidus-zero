bfbe38ebfe80748a0f678484406aa984d2b9af39
BLAKE2Share <dev@easymine.rocks>
2018-10-20 12:21:26 -0400

    Zero Update to Iquidus
    Update to Zero v1.0.2 for testing

--- scripts/peers.js
+++ scripts/peers.js
@@ -6,6 +6,14 @@ var mongoose = require('mongoose')
 
 var COUNT = 5000; //number of blocks to index
 
+function maskips (splitaddy) {
+  var ip = splitaddy;
+  var breakip = ip.split('.');
+  var masked = breakip[0] + '.' + breakip[1] + '.' + 'XXX' + '.' + 'XXX';
+//  console.log(breakip[0] + '.' + breakip[1] + '.' + 'XXX' + '.' + 'XXX');
+  return masked;
+}
+
 function exit() {
   mongoose.disconnect();
   process.exit(0);
@@ -27,17 +35,19 @@ mongoose.connect(dbString, function(err) {
       lib.syncLoop(body.length, function (loop) {
         var i = loop.iteration();
         var address = body[i].addr.split(':')[0];
-        db.find_peer(address, function(peer) {
+	var maskedaddy = maskips(address);
+        db.find_peer(maskedaddy, function(peer) {
           if (peer) {
             // peer already exists
             loop.next();
           } else {
             request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
               db.create_peer({
-                address: address,
+                address: maskedaddy,
                 protocol: body[i].version,
                 version: body[i].subver.replace('/', '').replace('/', ''),
-                country: geo.country_name
+                country: geo.country_name,
+                countrycode: geo.country_code
               }, function(){
                 loop.next();
               });
